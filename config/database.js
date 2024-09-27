f184b06c61333ef0f921cdb62d4e44466a337a22
OswaldMogot <oswaldmogot235@gmail.com>
2024-09-26 19:01:40 -0700

    nambah post dan user router

---
+++ config/database.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2";
+
+
+const database = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "uas_database"
+});
+
+export default database.promise();
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userRouter from "./routers/userRouter.js";
+import postRouter from "./routers/postRouter.js";
+
+
+const app = express();
+
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
